e8daadbe9910d7a4de3bf1a78b42ddb63c83ef8b
EC2 Default User <youcanybks3652@gmail.com>
2020-07-19 08:08:08 +0000

    PHP/Laravel 13 ニュース投稿画面を作成しよう　の課題

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -18,6 +18,54 @@
             <div class="row">
                 <div class="col-md-8 mx-auto">
                     <h2>MY プロフィール</h2>
+                    <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+                        
+                        @if (count($errors) > 0)
+                            <ul>
+                                @foreach($errors->all() as $e)
+                                    <li>{{ $e }}</li>
+                                @endforeach    
+                            </ul>
+                        @endif
+                    <div class="form-group row">
+                        <label class="col-md-2">名前</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="mame" value="{{ old('title') }}"　placeholder="名前">
+                        </div>
+                    </div>
+                    
+                     <div class="form-group row">
+                        <label class="col-md-2">性別</label>
+                          <div class="form-check form-check-inline">
+                              <input class="form-check-input" type="radio" name="gender" id="man" value="A">
+                              <label class="form-check-label" for="radio2a">男</label>
+                        　</div>
+                        　<div class="form-check form-check-inline">
+                              <input class="form-check-input" type="radio" name="gender" id="woman" value="B">
+                              <label class="form-check-label" for="radio2b">女</label>
+                        　</div>
+                   　</div>
+                   　
+                       　<div class="form-group row">
+                            <label class="col-md-2">趣味</label>
+                            <div class="col-md-10">
+                                <textarea class="form-control" name="hobby" >{{ old('hobby') }}</textarea>
+                            </div>
+                        </div>
+                    　
+                    　<div class="form-group row">
+                        <label class="col-md-2">自己紹介欄</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
+                        </div>
+                    </div>
+                    
+                    
+                    {{ csrf_field() }}
+                    <div class="form-group row">
+                    <input type="submit" class="btn btn-primary col-md-4" value="送信">
+                    </div>
+                    </form>
                 </div>
             </div>
         </div>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -20,6 +20,14 @@
         <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
         {{-- この章の後半で作成するCSSを読み込みます --}}
         <link href="{{ secure_asset('css/admin.css') }}" rel="stylesheet">
+        
+      
+
+<!-- JS, Popper.js, and jQuery -->{{-- CDNでBootstrapのJSを読み込ませる --}}
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+        
     </head>
     <body>
         <div id="app">
@@ -48,16 +56,19 @@
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown"> 
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
+                                    
+                                
+                                    
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
+                                       {{ csrf_field() }}
                                     </form>
-                                </div>
+                                </div> 
                             </li>
                         @endguest
                         </ul>
@@ -69,6 +80,11 @@
                 {{-- コンテンツをここに入れるため、@yieldで空けておきます。 --}}
                 @yield('content')
             </main>
+            
+             
+
+
+            
         </div>
     </body>
 </html>
